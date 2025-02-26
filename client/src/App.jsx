b0ca19ddb3ac05ff17f9d1a9d711234449529a9b
Prabal Bhandary <prabalbhandary97@gmail.com>
2025-02-26 15:24:50 +0545

    routes & antd style

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import "./App.css"
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 const App = () => {
   return (
     <>
-      <div className="bg-dark">
-        <h1 className='text-danger'>Hello</h1>
-      </div>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </Router>
     </>
   )
 }
